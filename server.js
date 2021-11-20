b1c6cd257b22376f44623b429fad086e0284ac31
Titoxox97 <87781987+Titoxox97@users.noreply.github.com>
2021-11-20 15:28:52 -0500

    server express routes

--- server.js
+++ server.js
@@ -3,14 +3,27 @@ const express = require("express");
 const express_hbs = require("express-handlebars");
 const session = require(`express-session`);
 const { Sequelize } = require("sequelize/types");
+const sequelize = require("./02-Challenge/config/connection");
 
 var SequelizeStore = require("connect-session-sequelize")(session.Store);
 
-const sequelize = new Sequelize("comment", "post", "user", {
-  dialect: "sqlite",
-  storafe: "./session.sqlite",
-});
-
 const app = express();
-app.use(session());
 const PORT = process.env.PORT || 3001;
+
+app.use(
+  session({
+    secret: "Top Secret",
+    store: new SequelizeStore({
+      db: sequelize,
+    }),
+  })
+);
+
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(require("02-Challenge/routes.js"));
+
+app.listen(PORT, () => {
+  console.log("App listening on PORT " + PORT);
+});
